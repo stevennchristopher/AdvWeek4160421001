ed2cf9bf710c0b2996dad289515895b0f7ecd7ba
stevennchristopher <stchrist03@gmail.com>
2024-03-21 14:25:49 +0700

    w5

--- app/src/main/java/com/example/advweek4/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/advweek4/viewmodel/ListViewModel.kt
@@ -1,22 +1,62 @@
 package com.example.advweek4.viewmodel
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.advweek4.model.Student
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
-class ListViewModel:ViewModel() {
+class ListViewModel(application: Application): AndroidViewModel(application){
     val studentsLiveData = MutableLiveData<ArrayList<Student>>()
     val studentLoadErrorLD = MutableLiveData<Boolean>()
     val loadingLD = MutableLiveData<Boolean>()
 
+    val TAG = "volleyTag"
+    private var queue: RequestQueue? = null
+
     fun refresh() {
-        studentsLiveData.value = arrayListOf(
-            Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff"),
-            Student("13312","Rich","1994/12/14","3925444073","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff"),
-            Student("11204","Dinny","1994/10/07","6827808747","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff1")
-        )
+//        studentsLiveData.value = arrayListOf(
+//            Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff"),
+//            Student("13312","Rich","1994/12/14","3925444073","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff"),
+//            Student("11204","Dinny","1994/10/07","6827808747","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff1")
+//        )
 
         studentLoadErrorLD.value = false
-        loadingLD.value = false
+        loadingLD.value = true
+
+        queue = Volley.newRequestQueue( getApplication() )
+        val url = "http://adv.jitusolution.com/student.php"
+
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            {
+                val sType = object : TypeToken<List<Student>>() { }.type
+                val result = Gson().fromJson<List<Student>>(it, sType)
+                studentsLiveData.value = result as ArrayList<Student>?
+                loadingLD.value = false
+
+                Log.d("showvoley", it)
+            },
+            {
+                Log.d("showvoley", it.toString())
+                studentLoadErrorLD.value = false
+                loadingLD.value = false
+            })
+
+        stringRequest.tag = TAG
+        queue?.add(stringRequest)
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        queue?.cancelAll(TAG)
+    }
+
 }
